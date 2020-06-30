1d9f6724db13d5dac35abc94c673a1430eb9f421
Parham Alvani <parham.alvani@gmail.com>
2020-06-30 12:01:59 +0430

    feat: Update UDP Noise Command Line

--- cmd/udpnoise/main.go
+++ cmd/udpnoise/main.go
@@ -1,13 +1,3 @@
-/*
- * +===============================================
- * | Author:        Parham Alvani <parham.alvani@gmail.com>
- * |
- * | Creation Date: 02-05-2018
- * |
- * | File Name:     main.go
- * +===============================================
- */
-
 package main
 
 import (
@@ -15,21 +5,25 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/aut-ceit/UDPNoise/udpnoise"
+	"github.com/elahe-dastan/UDPNoise/udpnoise"
 )
 
 func main() {
+	fmt.Println("Elahe Dastan <elahe.dstn@gmail.com>")
 	fmt.Println("Parham Alvani <parham.alvani@gmail.com>")
 
 	var loss = flag.Int("loss", 0, "packet loss ratio")
+
 	var destination = flag.String("destination", "127.0.0.1:8080", "udp packets destination")
+
 	flag.Parse()
 
 	u, err := udpnoise.New(*loss, *destination)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("UDP Noise on :%d -> %s with %d loss\n", u.Port, u.Destination, u.Loss)
+
+	fmt.Printf("UDP Noise on :%d <---> %s with %d loss\n", u.Port, u.Destination, u.Loss)
 
 	u.Run()
 }
